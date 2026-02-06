800a5a953d8c3ea5af191a2f1cb5dd6bb51e45d1
jumcityzaza <87872058+jumcityzaza@users.noreply.github.com>
2026-02-06 15:04:51 +0700

    Update settings.py

--- azure_project/settings.py
+++ azure_project/settings.py
@@ -106,8 +106,8 @@ DATABASES = {
 import urllib.parse
 
 # MongoDB Configuration
-mongo_username = urllib.parse.quote_plus('bearlab')
-mongo_password = urllib.parse.quote_plus('P@ss1234')
+mongo_username = urllib.parse.quote_plus('jummumadmin')
+mongo_password = urllib.parse.quote_plus('Hueningkai1')
 # MONGO_URI = 'mongodb://localhost:27017/'
 MONGO_URI = f'mongodb+srv://{mongo_username}:{mongo_password}@hai-mongodb.global.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000'
 MONGO_DB_NAME = 'hai-mongodb'
